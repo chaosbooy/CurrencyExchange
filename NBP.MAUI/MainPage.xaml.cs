c80111cab0e464b1d8ee6ef56d9671a1410363b7
chaosbooy <lpmelon234@gmail.com>
2024-05-28 15:25:02 +0200

    kantor ostatnie cos
    tam duzo naras nwm nei chce mi sie opisywac

--- NBP.MAUI/MainPage.xaml.cs
+++ NBP.MAUI/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using NBP.MAUI.Resources.Scripts;
+using Newtonsoft.Json;
 using System.Net.Http.Headers;
 
 namespace NBP.MAUI
@@ -8,12 +9,17 @@ namespace NBP.MAUI
         private List<string> currencies;
         private RootObject allRates;
         List<string> pickersList;
+        thirtyDays ostatnieDni = new thirtyDays();
+        private Drawing d;
 
         public MainPage()
         {
             pickersList = new List<string>();
             InitializeComponent();
             InitializeClass();
+
+            d = new Drawing(300, 500);
+            graph.Drawable = d;
         }
 
         private async void InitializeClass()
@@ -51,7 +57,7 @@ namespace NBP.MAUI
                 allRates.rates.Add(new Rate
                 {
                     code = "GOLD",
-                    currency = "światowe",
+                    currency = "(mierzone w jabłkach) światowe",
                     mid = goldRate.cena,
                 });
             }
@@ -66,6 +72,9 @@ namespace NBP.MAUI
 
             picker1.SelectedItem = pickersList[pickersList.Count - 2];
             picker2.SelectedItem = pickersList[pickersList.Count - 1];
+
+            label1.Text = allRates.rates.Find(x => x.code == picker1.SelectedItem).currency;
+            label2.Text = allRates.rates.Find(x => x.code == picker2.SelectedItem).currency;
 
         }
 
@@ -114,6 +123,13 @@ namespace NBP.MAUI
             var s = (Picker)sender;
             bool isFirstPicker = s == picker1;
 
+            if (isFirstPicker) label1.Text = allRates.rates.Find(x => x.code == s.SelectedItem).currency;
+            else
+            {
+                UpdateGraph();
+                label2.Text = allRates.rates.Find(x => x.code == s.SelectedItem).currency;
+            }
+
             if ((isFirstPicker && entry1.Text.Length == 0) || (!isFirstPicker && entry2.Text.Length == 0)) return;
 
             double firstMid = allRates.rates.Find(x => x.code == picker1.SelectedItem).mid;
@@ -122,15 +138,68 @@ namespace NBP.MAUI
             if (isFirstPicker)
                 convertedValue = double.Parse((entry1.Text.Length > 0 && entry1.Text[entry1.Text.Length - 1] == ',') ? entry1.Text.Substring(0, entry1.Text.Length - 1) : entry1.Text);
             else
-                convertedValue = double.Parse((entry2.Text.Length > 0 && entry2.Text[entry1.Text.Length - 1] == ',') ? entry2.Text.Substring(0, entry2.Text.Length - 1) : entry2.Text);
+                convertedValue = double.Parse((entry2.Text.Length > 0 && entry2.Text[entry2.Text.Length - 1] == ',') ? entry2.Text.Substring(0, entry2.Text.Length - 1) : entry2.Text);
 
             double exchangedValue = convertedValue / (!isFirstPicker ? firstMid : secondMid) * (!isFirstPicker ? secondMid : firstMid);
             if (isFirstPicker)
                 entry2.Text = $"{exchangedValue}";
             else
+            {
                 entry1.Text = $"{exchangedValue}";
+                UpdateGraph();
+            }
 
+        }
 
+        private async void UpdateGraph()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:64195/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            if (picker2.SelectedIndex == pickersList.Count - 2)
+            {
+                d.jakiKurs = "";
+                return;
+            }
+
+            d.jakiKurs = picker2.SelectedItem.ToString();
+            d.dniKursu.Clear();
+
+            if (picker2.SelectedIndex == pickersList.Count - 1)
+            {
+                var goldResponse = await client.GetAsync("http://api.nbp.pl/api/cenyzlota/last/30");
+                string response = await goldResponse.Content.ReadAsStringAsync();
+                var allDays = JsonConvert.DeserializeObject<List<thirtyGolds>>(response);
+
+                foreach (var day in allDays)
+                {
+                    d.dniKursu.Add(day.cena);
+                }
+
+            }
+            else
+            {
+                var currencyResponse = await client.GetAsync($"https://api.nbp.pl/api/exchangerates/rates/a/{d.jakiKurs.ToLower()}/last/30/");
+                string response = await currencyResponse.Content.ReadAsStringAsync();
+
+                x.Text = response;
+                var allDays = JsonConvert.DeserializeObject<thirtyDays>(response);
+
+                foreach (var day in allDays.rates)
+                {
+                    d.dniKursu.Add(day.mid);
+                }
+            }
+            x.Text = "";
+            foreach (var d in d.dniKursu)
+            {
+                x.Text += $"{d}   ";
+            }
+
+            graph.Invalidate();
         }
     }
     public class Rate
@@ -157,7 +226,6 @@ namespace NBP.MAUI
             cena = 0;
         }
     }
-
     public class RootObject
     {
         public string table { get; set; }
@@ -173,5 +241,43 @@ namespace NBP.MAUI
             rates = new();
         }
     }
+    public class thirtyDays
+    {
+        public string table { get; set; }
+        public string currency { get; set; }
+        public string code { get; set; }
+        public List<rate> rates { get; set; }
 
+        public thirtyDays()
+        {
+            table = "";
+            currency = "";
+            code = "";
+            rates = new();
+        }
+    }
+    public class rate
+    {
+        public string no { get; set; }
+        public string effectiveDate { get; set; }
+        public double mid { get; set; }
+
+        public rate()
+        {
+            no = "";
+            effectiveDate = "";
+            mid = 0;
+        }
+    }
+    public class thirtyGolds
+    {
+        public string data { get; set; }
+        public double cena { get; set; }
+
+        public thirtyGolds() 
+        {
+            data = "";
+            cena = 0;
+        }
+    }
 }
---
+++ NBP.MAUI/Resources/Scripts/Drawing.cs
@@ -0,0 +1,54 @@
+﻿namespace NBP.MAUI.Resources.Scripts
+{
+    internal class Drawing : IDrawable
+    {
+        public List<double> dniKursu = new();
+        public string jakiKurs = "";
+        private int offset = 50;
+        private float height;
+        private float width;
+        private float diagramWidth = 0;
+
+        public Drawing(float h, float w)
+        {
+            height = h;
+            width = w;
+        }
+
+        public void Draw(ICanvas canvas, RectF rect)
+        {
+            float diagramHeight = 200;
+            diagramWidth = width - 2 * offset;
+
+            canvas.StrokeColor = Colors.Black;
+            canvas.StrokeSize = 5;
+            canvas.FontSize = 30;
+
+            if (dniKursu.Count == 0)
+            {
+                canvas.DrawLine(offset, height - offset / 2, offset, height - offset - diagramHeight);
+                canvas.DrawLine(offset, height - offset / 2, width - offset, height - offset / 2);
+
+                return;
+            }
+
+            canvas.DrawString(jakiKurs, width / 2, offset / 2, HorizontalAlignment.Center);
+
+            canvas.DrawLine(offset, height - offset / 2, offset, height - offset - diagramHeight);
+            canvas.DrawLine(offset, height - offset / 2, width - offset, height - offset / 2);
+
+            double max = dniKursu.Max();
+            float heightSpacing = diagramHeight / (float)max;
+            float widthSpacing = diagramWidth / 30;
+
+            canvas.FontSize = 15;
+            canvas.DrawString(max.ToString("0.00"), offset, height - offset - diagramHeight, HorizontalAlignment.Right);
+            canvas.DrawString("0.00", offset, height - offset / 2, HorizontalAlignment.Right);
+
+            for(int i = 0; i < 30; i++)
+            {
+                canvas.DrawCircle(new PointF(offset + (widthSpacing * (i + 1)),(height - offset / 2) - (float)(heightSpacing * dniKursu[i])), 3);
+            }
+        }
+    }
+}
